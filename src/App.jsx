c77a35773ae502fcd9439e25b23a5ef3087f9bc6
the-hyperoot <rajesh.kumar.das.tech@gmail.com>
2023-01-15 06:05:49 +0530

    test

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Homepage from "./components/homepage";
+import BlogRedirect from "./components/blog-redirect";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <BlogRedirect /> */}
+        < Homepage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/blog-redirect.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+export default function BlogRedirect() {
+
+    return (
+        <>
+            <h1>To read my Blogs, click below!!</h1>
+            <a className="App-link" href="https://blog.hyperoot.live">
+                Blogs
+            </a>
+        </>
+    )
+}
